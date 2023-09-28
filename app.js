d078ec303ffa1be128898efe9eb730f027c24598
Dorian Kita <dorian.kita90@gmail.com>
2023-09-28 19:54:08 +0200

    added Library class with add book method

--- app.js
+++ app.js
@@ -3,15 +3,45 @@ class Book{
     #author;
     #publishYear;
 
-    constructor(title,author,publishYear){
+    constructor(title,author = 'none' ,publishYear = 'none'){
         this.#author = author;
         this.#title = title;
         this.#publishYear = publishYear;
     }
 
     showBook(){
-        console.log(`tytuł: ${this.#title} , Autor: ${this.#author}, Data wydania: ${this.#publishYear} `);
+        console.log(`Title: ${this.#title} , Author: ${this.#author}, Release date: ${this.#publishYear} `);
     }
 }
 
-class Library{}
+
+
+class Library{
+    #bookCollection;
+
+    constructor(){
+        this.#bookCollection = new Map();
+        this.init();
+    }
+
+    init(){
+        document.getElementById('add-book').addEventListener('click', () => this.addBook());
+    }
+
+    addBook(){
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const date = document.getElementById('date').value;
+
+        const book = new Book(title,author,date);
+        this.#bookCollection.set(title, book);
+
+        alert('Book has been added to library collection');
+        console.log(this.#bookCollection);
+    }
+}
+const library = new Library();
+
+// const book = new Book('Mira', 'Herkules', '2022');
+// console.log(book);
+// book.showBook();
